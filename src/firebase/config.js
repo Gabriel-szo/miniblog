9f6913032f3d3ce2e3ab331fb935c5cbe6dc2068
doublejumpz <zumbivirtual00@gmail.com>
2023-07-07 20:01:07 -0300

    Começando a construir o miniblog

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCtEgttW1Nz3Z3pRktWaM9pH02iXzKV8VM",
+  authDomain: "miniblog-a649c.firebaseapp.com",
+  projectId: "miniblog-a649c",
+  storageBucket: "miniblog-a649c.appspot.com",
+  messagingSenderId: "966885767092",
+  appId: "1:966885767092:web:214a11b9515238403ee4e1",
+};
+
+const app = initializeApp(firebaseConfig);
